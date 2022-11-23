b542b0ae435e51d0177a7c78f314beae42aab4d6
RomanG-sky <89301585+RomanG-sky@users.noreply.github.com>
2022-11-23 18:41:06 +0200

    1

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -80,7 +80,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+#  'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'my_database',
+#         'USER': 'root',
+#         'PASSWORD': '123456789Q',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
